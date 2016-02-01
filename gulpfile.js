77ea07d23fcc7f390691b696d819975c5686e1a1
Judit Bauer <judit.bauer@yahoo.com>
2016-02-01 13:49:58 +0100

    add gulp and jshint files

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var jshint = require('gulp-jshint');
+var gulp   = require('gulp');
+gulp.task('default', function() {
+  console.log('hello gulp');
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
+});

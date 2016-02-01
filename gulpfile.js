39b16adbb5bcf2fcc407ad0948ed99dcc3ca5ae3
Judit Bauer <judit.bauer@yahoo.com>
2016-02-01 14:12:46 +0100

    add jshint stylish

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
+var stylish = require('jshint-stylish');
 
 gulp.task('default', function() {
   console.log('hello gulp');
@@ -10,6 +11,6 @@ gulp.task('default', function() {
 gulp.task('lint', function() {
   return gulp.src('./lib/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
+  	.pipe(jshint.reporter(stylish))
 });
 

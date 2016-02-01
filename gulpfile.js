dd8401154838141a58377c0a0df8f29917765852
Ákos Molnár <molak06@gmail.com>
2016-02-01 13:38:55 +0100

    addded gulp + jshint + gulpjshint

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var jshint = require('gulp-jshint');
+var gulp   = require('gulp');
+
+gulp.task('default', function() {
+  console.log('hello gulp');
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
+});

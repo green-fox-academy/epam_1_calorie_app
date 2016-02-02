4b10f154d400c564223af246ebe979577c6ced96
Judit Bauer <judit.bauer@yahoo.com>
2016-02-02 10:14:49 +0100

    disabled app.get function

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.listen(port, function() {
 	console.log('Listening on port 3000...')
 });
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
 	res.send('Hello world');
-});
+});*/

d6d86c33630bd31b1a13fb3a7efbd174c59e084d
Ákos Molnár <molak06@gmail.com>
2016-02-02 10:44:41 +0100

    feat(added post and get functions to server.js to test heroku responses)

--- server/server.js
+++ server/server.js
@@ -3,15 +3,24 @@
 
 var express = require('express');
 var app = express();
+var items = require('./items');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
+items.addMeal({name: 'csoki', calorie: '420'});
+items.addMeal({name: 'kavezzunk pls', calorie: '50'});
 
 app.use(express.static('public'));
 
-app.listen(port, function() {
-	console.log('Listening on port 3000...')
+app.get('/meals', function(req, res) {
+	res.json(items.allMeals());
 });
 
-app.get('/', function(req, res) {
-	res.send('Hello world');
+app.post('/meals', function(req, res) {
+	var meal = items.addMeal(req.body);
+	res.status(201).json(item);
+}
+
+app.listen(port, function() {
+	console.log('Listening on port 3000...');
 });

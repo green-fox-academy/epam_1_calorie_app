18ae6b655da403240bff599dba82999ead74c6a0
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-02-02 13:21:27 +0100

    test database for rows

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send('Error ' + err); }
       else
-       { response.json(result); }
+       { response.json(result.rows); }
     });
   });
 });

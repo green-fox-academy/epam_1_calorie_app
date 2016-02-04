5db403c5bcd0e34845a8a47484fcb5fd9e19dd20
Judit Bauer <judit.bauer@yahoo.com>
2016-02-04 15:04:28 +0100

    add test for deleteItem and addItem functions

--- spec/itemsSpec.js
+++ spec/itemsSpec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var SQL = require('sql-template-strings');
+
 describe('Meals', function() {
 
   describe('getAll()', function() {
@@ -16,4 +18,43 @@ describe('Meals', function() {
     });
   });
 
+  describe('addItem()', function() {
+    it('tracks all the arguments of its calls', function() {
+      var connection = {};
+      connection.sendQuery = function (query, callback) {
+        return callback(null, [{}]);
+      };
+      var Meals = require('../server/items');
+      var meals = new Meals(connection);
+      var params = {name: 'name', calories: 'calories', date: 'date'};
+      spyOn(connection, 'sendQuery');
+      meals.addItem(params,'callback');
+      expect(connection.sendQuery).toHaveBeenCalledWith(SQL`
+      INSERT INTO meals (name, calories, date)
+      VALUES (${params.name}, ${params.calories}, ${params.date})
+      RETURNING id, name, calories, date`,
+      'callback');
+    });
+  });
+
+  describe('deletItem()', function() {
+    it('tracks all the arguments of its calls', function() {
+      var connection = {};
+      connection.sendQuery = function (query, callback) {
+        return callback(null, [{}]);
+      };
+      var Meals = require('../server/items');
+      var meals = new Meals(connection);
+      var id = '';
+      spyOn(connection, 'sendQuery');
+      meals.deleteItem(id, 'callback');
+      expect(connection.sendQuery).toHaveBeenCalledWith(SQL`
+      DELETE FROM meals WHERE id = ${id}
+      RETURNING id, name, calories, date`,
+      'callback');
+    });
+  });
+
 });
+
+

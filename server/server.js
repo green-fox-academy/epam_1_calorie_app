8a58ca5cdc806066074ca64fe8ac3adff0b64baa
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-10 11:20:19 +0100

    separate service functions from server.js

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ var bodyParser = require('body-parser');
 var config = require('./config.js');
 var path = require('path');
 
-var Meals = require('./items.js');
+var Queries = require('./queries.js');
 var Connection = require('./connection.js');
 var connection = new Connection();
-var meals = new Meals(connection);
+var queries = new Queries(connection);
+var Service = require('./service.js');
+var service = new Service(queries);
 
 var port = process.env.PORT || config.defaultPort;
 var app = express();
@@ -21,33 +23,6 @@ app.listen(port, function() {
 	console.log('Listening on port ' + port + '...');
 });
 
-app.get('/meals', getAll);
-app.post('/meals', addItem);
-app.delete('/meals/:id', deleteItem);
-
-function getAll(request, response) {
-	meals.getAll(function (err, result) {
-    handleResponse(err, result, response);
-	});
-}
-
-function addItem(request, response) {
-	meals.addItem(request.body, function(err, result) {
-		handleResponse(err, result, response);
-	});
-}
-
-function deleteItem(request, response) {
-	meals.deleteItem(request.params.id, function(err, result) {
-		handleResponse(err, result, response);
-	});
-}
-
-function handleResponse(err, result, response) {
-	if (err) {
-		console.error(err);
-		response.send('Error ' + err);
-	}	else {
-		response.json(result.rows);
-	}
-}
+app.get('/meals', service.getAll);
+app.post('/meals', service.addItem);
+app.delete('/meals/:id', service.deleteItem);

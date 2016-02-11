3590f9747be6682021032e7ea319453a30dbfcf2
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-02-11 15:40:30 +0100

    Test addItem and deleteItem requests

--- spec/frontendSpec/httpSpec.js
+++ spec/frontendSpec/httpSpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-describe('', function() {
+describe('test http requests', function() {
 
   beforeEach(module('CalorieApp'));
 
@@ -9,10 +9,11 @@ describe('', function() {
 
   beforeEach(inject(function(_meals_, _$httpBackend_, _$location_) {
     meals = _meals_;
-    console.log(meals);
     $httpBackend = _$httpBackend_;
     $location = _$location_;
-    $httpBackend.when('GET', $location.absUrl() + '/meals').respond([{}]);
+    $httpBackend.when('GET', $location.absUrl() + 'meals/').respond([{}]);
+    $httpBackend.when('POST', $location.absUrl() + 'meals/').respond([{}]);
+    $httpBackend.when('DELETE', $location.absUrl() + 'meals/1').respond([{}]);
   }));
 
   afterEach(function() {
@@ -24,12 +25,43 @@ describe('', function() {
     expect(meals.list.length).toBe(0);
   });
 
-  it('should have a empty list', function() {
-    console.log($httpBackend);
-    $httpBackend.expectGET($location.absUrl() + '/meals');
+  it('should return the list', function() {
+    $httpBackend.expectGET($location.absUrl() + 'meals/');
     meals.getAll();
     $httpBackend.flush();
     expect(meals.list).toEqual([{}]);
   });
 
+  it('the list shouldn\'t be empty after add', function() {
+    var meal = {
+      id: 1,
+      name: 'apple',
+      calories: 200,
+      date: 0
+    };
+
+    $httpBackend.expectPOST($location.absUrl() + 'meals/', meal);
+    meals.addItem(meal);
+    $httpBackend.flush();
+    expect(meals.list.length).toBe(1);
+  });
+
+  it('the list should be empty after add and delete', function() {
+    var meal = {
+      id: 1,
+      name: 'apple',
+      calories: 200,
+      date: 0
+    };
+
+    $httpBackend.expectPOST($location.absUrl() + 'meals/', meal);
+    meals.addItem(meal);
+    $httpBackend.flush();
+    expect(meals.list.length).toBe(1);
+
+    $httpBackend.expectDELETE($location.absUrl() + 'meals/'+ meal.id);
+    meals.deleteItem(meal);
+    $httpBackend.flush();
+    expect(meals.list.length).toBe(0);
+  });
 });

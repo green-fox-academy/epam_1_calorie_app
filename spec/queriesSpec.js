84f5e1cffea3433f537dc15fc5e3d03baa00858c
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-02-12 12:31:03 +0100

    Add callback function and callThrough to spy

--- spec/queriesSpec.js
+++ spec/queriesSpec.js
@@ -6,43 +6,43 @@ describe('Meals', function() {
     var connection = {};
     var Meals = require('../server/meals_queries');
     var meals = new Meals(connection);
+    var callback;
 
   beforeEach(function() {
     connection.sendQuery = function (query, callback) {
       return callback(null, [{}]);
     };
+    callback = function(){};
+    spyOn(connection, 'sendQuery').and.callThrough();
   });
 
   describe('getAll()', function() {
     it('tracks all the arguments of its calls', function() {
-      spyOn(connection, 'sendQuery');
-      meals.getAll('callback');
-      expect(connection.sendQuery).toHaveBeenCalledWith('SELECT id, name, calories, date FROM meals', 'callback');
+      meals.getAll(callback);
+      expect(connection.sendQuery).toHaveBeenCalledWith('SELECT id, name, calories, date FROM meals', callback);
     });
   });
 
   describe('addItem()', function() {
     it('tracks all the arguments of its calls', function() {
       var params = {name: 'name', calories: 'calories', date: 'date'};
-      spyOn(connection, 'sendQuery');
-      meals.addItem(params,'callback');
+      meals.addItem(params, callback);
       expect(connection.sendQuery).toHaveBeenCalledWith(SQL`
       INSERT INTO meals (name, calories, date)
       VALUES (${params.name}, ${params.calories}, ${params.date})
       RETURNING id, name, calories, date`,
-      'callback');
+      callback);
     });
   });
 
   describe('deletItem()', function() {
     it('tracks all the arguments of its calls', function() {
       var id = '';
-      spyOn(connection, 'sendQuery');
-      meals.deleteItem(id, 'callback');
+      meals.deleteItem(id, callback);
       expect(connection.sendQuery).toHaveBeenCalledWith(SQL`
       DELETE FROM meals WHERE id = ${id}
       RETURNING id, name, calories, date`,
-      'callback');
+      callback);
     });
   });
 
